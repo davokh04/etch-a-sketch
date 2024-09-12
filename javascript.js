8122f0d40871a0f7351464716e0cf6fb42fb32c5
David <davkh10@gmail.com>
2024-09-12 15:56:18 -0300

    Create rest of features and complete project

--- javascript.js
+++ javascript.js
@@ -1,15 +1,53 @@
-/* create initial 16x16 grid */
-const squareContainer = document.querySelector(".squareContainer");
-squareContainer.style.cssText = "display: flex; flex-wrap: wrap; border: 1px solid black; width: 480px;";
+const body = document.querySelector("body");
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.style.cssText = "height: 30px; flex: 0 0 30px; border: 1px solid black; box-sizing: border-box;";
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
-    });
-    squareContainer.appendChild(square);
+
+function createDisplay() {
+    const squareContainer = document.createElement("div");
+    squareContainer.classList.add("squareContainer");
+    const screenHeightWidth = 480;
+    squareContainer.style.cssText = `display: flex; flex-wrap: wrap; border: 2px solid black; width: ${screenHeightWidth}px;`;
+    body.appendChild(squareContainer);
+    return squareContainer
 };
+
+
+
+function createGrid(squareContainer, squaresPerRow=16) {
+
+    while (squareContainer.firstChild) {
+        squareContainer.removeChild(squareContainer.firstChild);
+    };
+
+    let styles = window.getComputedStyle(squareContainer);
+    let screenHeightWidth = parseInt(styles.getPropertyValue("width"));
+    for (let i = 0; i < squaresPerRow**2; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.cssText = `height: ${screenHeightWidth/squaresPerRow}px; flex: 0 0 ${screenHeightWidth/squaresPerRow}px;`;
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        });
+        squareContainer.appendChild(square);
+    };
+};
+
+
+const display = createDisplay()
+createGrid(display);
+
+
+const inputButton = document.createElement("button");
+inputButton.textContent = "Click me to choose the squares per row";
+body.prepend(inputButton);
+inputButton.addEventListener("click", () => {
+    let input = 0;
+    while ((input % 1 != 0) || (input == 0) || (input > 100)) {
+        input = parseFloat(prompt());
+
+    };
+    createGrid(display, input);
+});
+
+
 
 
